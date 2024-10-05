780101aec17fb31d1a8fc0f8142f7f1c702547a4
DashadotT <markjovanoctat@gmail.com>
2024-10-05 15:35:19 +0800

    L02-Added Object with values and display via console.log

---
+++ L02_Methods.js
@@ -0,0 +1,14 @@
+//Input
+const person1 = {
+    name: "Mark Jovan Octat",
+    age: 19,
+    gender: "Male",
+    hobby: "Playing instruments",
+}
+
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Gender: "+person1.gender);
+console.log("Hobby: "+person1.hobby);
+console.log("--------------------------------");
